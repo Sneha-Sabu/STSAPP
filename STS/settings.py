cec79969d75fd77cacb91799fc138e7160623428
t17ss19 <t17ss19@abdn.ac.uk>
2020-07-29 21:11:32 +0100

    Settings file update

--- STS/settings.py
+++ STS/settings.py
@@ -132,11 +132,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    ("admin", "/static/admin/css"),  os.path.join(BASE_DIR, 'static')
+    ("admin", "/static/admin/css"),
 ]
 
 MEDIA_URL = '/images/'
